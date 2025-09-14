bbb763a3eb9d436080424f94f5e983c0669eecb3
nu1ptr <j6quach@gmail.com>
2025-09-13 21:09:29 -0500

    working matmul naive with grada,gradb,gradcheck

--- j6q_cu_ext/csrc/cuda/matmul_naive.cu
+++ j6q_cu_ext/csrc/cuda/matmul_naive.cu
@@ -33,12 +33,18 @@ namespace j6q_cu_ext{
             int x = (blockDim.x * blockIdx.x) + threadIdx.x;
             int y = (blockDim.y * blockIdx.y) + threadIdx.y;
 
-            if(x < n && y < m) return;
+            // grad_out -> (m,n)
+            // a -> (m,k)
+            // b -> (k,n)
+            // da = grad_out @ b.T -> (m,n) @ (n,k) = (m,k)
+            if(x >= k || y >= m) return;
 
             scalar_t sum = scalar_t(0);
-            for(int i = 0; i < k; i++){
+            for(int i = 0; i < n; ++i){
+                sum += grad_out[y*n + i]*b[x*n + i];
             }
-            da[y*n + x] = sum;
+
+            da[y*k + x] = sum;
     }
 
     template<typename scalar_t>
@@ -47,8 +53,18 @@ namespace j6q_cu_ext{
             int x = (blockDim.x * blockIdx.x) + threadIdx.x;
             int y = (blockDim.y * blockIdx.y) + threadIdx.y;
 
-            if(x < n && y < m) return;
-            db[y*n + x] = 0;
+            // grad_out -> (m,n)
+            // a -> (m,k)
+            // b -> (k,n)
+            // db = a.T @ grad_out = (k,m) @ (m,n) = (k,n)
+            if(x >= n || y >= k) return;
+
+            scalar_t sum = scalar_t(0);
+            for(int i = 0; i < m; ++i){
+                sum += a[i*k + y]*grad_out[i*n + x];
+            }
+
+            db[y*n + x] = sum;
     }
 }
 
@@ -93,17 +109,19 @@ std::tuple<at::Tensor, at::Tensor> matmul_naive_backward(
     TORCH_INTERNAL_ASSERT(a.device().type() == at::DeviceType::CUDA);
     TORCH_INTERNAL_ASSERT(b.device().type() == at::DeviceType::CUDA);
 
-    TORCH_CHECK(b.size(0) == k, "Rows of a must equal columns of b");
+    TORCH_CHECK(b.size(0) == k, "Shape mismatch");
 
     at::Tensor a_contig = a.contiguous();
     at::Tensor b_contig = b.contiguous();
     at::Tensor grad_out_contig = grad_out.contiguous();
 
-    at::Tensor da = torch::empty({m,n}, a_contig.options());
-    at::Tensor db = torch::empty({m,n}, a_contig.options());
+    at::Tensor da = torch::empty_like(a, a_contig.options());
+    at::Tensor db = torch::empty_like(b, a_contig.options());
 
-    dim3 blockDim(16,16);
-    dim3 gridDim((n + 15) / 16, (m + 15) / 16);
+    dim3 da_blockDim(16,16);
+    dim3 da_gridDim((k + 15) / 16, (m + 15) / 16);
+    dim3 db_blockDim(16,16);
+    dim3 db_gridDim((n + 15) / 16, (k + 15) / 16);
 
     cudaStream_t stream = at::cuda::getCurrentCUDAStream();
     AT_DISPATCH_FLOATING_TYPES(a.scalar_type(), "matmul_naive", [&]{
@@ -114,11 +132,11 @@ std::tuple<at::Tensor, at::Tensor> matmul_naive_backward(
         scalar_t* da_ptr = da.data_ptr<scalar_t>();
         scalar_t* db_ptr = db.data_ptr<scalar_t>();
 
-        j6q_cu_ext::matmul_naive_gradA_kernel<scalar_t><<<gridDim, blockDim, 0>>>(
+        j6q_cu_ext::matmul_naive_gradA_kernel<scalar_t><<<da_gridDim, da_blockDim, 0, stream>>>(
             grad_out_ptr, b_ptr, da_ptr, m, k, n);
         C10_CUDA_KERNEL_LAUNCH_CHECK();
 
-        j6q_cu_ext::matmul_naive_gradB_kernel<scalar_t><<<gridDim, blockDim, 0>>>(
+        j6q_cu_ext::matmul_naive_gradB_kernel<scalar_t><<<db_gridDim, db_blockDim, 0, stream>>>(
             grad_out_ptr, a_ptr, db_ptr, m, k, n);
         C10_CUDA_KERNEL_LAUNCH_CHECK();
     });
--- test/utils.py
+++ test/utils.py
@@ -1,3 +1,8 @@
+"""
+    Basic inputs for testing.
+    
+    If you are using gradcheck you're going to want to keep the tensors small.
+"""
 import torch
 
 def sample_1d_inputs(device, *, requires_grad=False):
@@ -12,12 +17,7 @@ def sample_1d_inputs(device, *, requires_grad=False):
 def sample_2d_inputs(device, *, requires_grad=False):
     def make_tensor(*size):
         return torch.randn(size, dtype=torch.float, device=device, requires_grad=requires_grad)
-            
 
     return [
         [make_tensor(32,32), make_tensor(32,32)],
-        [make_tensor(128,32), make_tensor(32,128)],
-        [make_tensor(256,64), make_tensor(64,256)],
-        [make_tensor(512,128), make_tensor(128,512)],
-        [make_tensor(1024,256), make_tensor(256,1024)],
     ]
