de052c2fd650f80d65824973e94b905982bac272
nu1ptr <j6quach@gmail.com>
2025-09-28 13:57:22 -0500

    CMake test case compilation support

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
 mkdir -p build && cd build
-cmake .. -DCMAKE_PREFIX_PATH=/home/jq/Packages/miniconda3/envs/cuda-kernels/lib/python3.11/site-packages/torch/
+cmake .. \
+    -DCMAKE_PREFIX_PATH=/home/jq/Packages/miniconda3/envs/cuda-kernels/lib/python3.11/site-packages/torch/ \
+    -DCMAKE_CUDA_ARCHITECTURES=86
+
 make -j
