7422902b97b08aa82225701ec7a09fe000209b4b
nu1ptr <j6quach@gmail.com>
2025-10-15 22:02:49 -0500

    grid/block switch

--- src/tensor/native/arange.cu
+++ src/tensor/native/arange.cu
@@ -10,8 +10,8 @@ namespace jqTen{
         float* a_buf = static_cast<float*>(a.data());
 
         // TODO: Change this soon.
-        dim3 gridDim = dim3(32);
-        dim3 blockDim = dim3((n - 1) / 32 + 1);
+        dim3 gridDim = dim3((n - 1) / 32 + 1);
+        dim3 blockDim = dim3(32);
         arange_kernel<float><<<gridDim, blockDim, 0>>>(a_buf, n);
 
         return a;
